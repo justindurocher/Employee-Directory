43b14bcf5c47373ea4ec0408098a5296c756c15c
Justin Durocher <justduro@gmail.com>
2020-04-11 21:54:03 -0400

    components

---
+++ employee-directory/src/components/DataArea/dataArea.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import SearchBox from "../SearchBox/SearchBox.js";
+import DataTable from "../DataTable/dataTable.js";
+import API from "../../utils/API";
+import "./dataArea.css";
