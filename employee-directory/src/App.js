0fa3f3201b0315d5d3085b50a61e4f4649db5d7f
Justin Durocher <justduro@gmail.com>
2020-04-13 23:10:59 -0400

    heroku

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import DataArea from './components/DataArea/DataArea.js'
 
 function App() {
+
   return (
-    <Container />
+
+    <div className="wholePage"> 
+
+      <Header />
+
+      <Container>
+
+        <Home />
+
+      </Container>
+
+    </div>
   );
+
 }
 
 export default App;
